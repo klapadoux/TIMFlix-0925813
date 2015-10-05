e9397189306736c32ecda5de50c1ffed961ec19f
klapadoux <klapadoux@hotmail.com>
2015-10-05 15:49:37 -0400

    debut

---
+++ TIMFLIX/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TIMFLIX
+//
+//  Created by Alain on 2014-07-16.
+//  Copyright (c) 2014 TIM. All rights reserved.
+//  ---------------------------------------------
+//  Projet de départ
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    // **********************************
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Votre code ...
+        
+    } // viewDidLoad
+
+
+} // class ViewController
+
